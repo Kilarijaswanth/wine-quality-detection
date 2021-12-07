6e93e8c5b68e86f40f3a84dc0145daa6af3fece5
Kilarijaswanth <87745741+Kilarijaswanth@users.noreply.github.com>
2021-12-07 21:18:05 +0530

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -24,3 +24,28 @@ X=df.drop('quality',axis=1)
 y=df['quality']
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=101)
 
+#linear regression
+from sklearn.linear_model import LinearRegression lr=LinearRegression()
+lr.fit(X_train,y_train) 
+#acurracy
+lr=lr.score(X_test, y_test)
+
+# random forest
+from sklearn.ensemble import RandomForestClassifier 
+rf=RandomForestClassifier(n_estimators=100)
+rf.fit(X_train,y_train)
+rf=rf.score(X_test,y_test)
+
+# decision tree
+from sklearn.tree import DecisionTreeClassifier 
+dt=DecisionTreeClassifier() 
+dt.fit(X_train,y_train)
+dt=dt.score(X_test,y_test)
+models = pd.DataFrame({ 'Model': ['Linear Regression','Random Forest','Decision Tree'], 'Score': [lr,rf,dt]})
+models.sort_values(by='Score', ascending=False)
+plt.figure(figsize=(10,5)) 
+sns.barplot(x='Model',y='Score',data=models) 
+plt.show()
+
+
+
